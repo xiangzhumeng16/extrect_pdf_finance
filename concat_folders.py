fd39d93fc9bdf0f0b5879e914ec827b792e93bb8
xiangzhumeng16 <47736787+xiangzhumeng16@users.noreply.github.com>
2019-07-22 08:41:11 +0800

    Update concat_folders.py

--- concat_folders.py
+++ concat_folders.py
@@ -2,20 +2,21 @@ import os
 import shutil
 import glob
 
-def error_files_management(file_name, fold='error_fold'):
+def move_file(file_name, fold='error_fold'):
     if not os.path.exists(fold):
         os.mkdir(fold)
     dest_file = os.path.join(fold, os.path.basename(file_name))
     shutil.move(file_name, dest_file)
     print(f"Succefully moving the file {file_name} into the target {dest_file} file.")
 
-def filter_errors(fold='error_fold'):
+def concat_folders(fold='error_fold'):
     for sub_dir in os.listdir(fold):
         for filename in glob.glob(os.path.join(fold, sub_dir)+'/*.txt'):
             if os.path.exists(filename):
-                error_files_management(filename, fold=fold)
+                move_file(filename, fold=fold)
+		os.removedirs(os.path.join(fold, sub_dir))
 
 if __name__ == "__main__":
     # filter_errors()
-    filter_errors('sample_2015_txt_split')
+    concat_folders('sample_2015_txt_split')
     
